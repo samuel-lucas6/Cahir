951bb815932780bcca240d6c206f4cfbbb069ac5
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2024-04-28 11:44:23 +0100

    PasswordEntry.cs: Remove the password fingerprint.
    This is a bad idea from a security perspective. Interactive password entry exists to a) hide the typed characters and b) clear memory as best as possible. If someone observes the fingerprint (e.g., shoulder surfing or CCTV), they can now brute force the master password much more efficiently. By contrast, observing a derived site password doesn't help brute force the master password if a pepper is used. Remembering emojis or a few words is much easier than remembering each key pressed on a keyboard, which can also be hidden from view.
    
    There are several ways to improve the current fingerprint approach:
    
    - Debouncing like in LessPass.
    - Using fewer words/emojis like in LessPass.
    - Using a password hashing algorithm whilst they're typing characters.
    - Deriving a fingerprint using a password hashing algorithm after they've submitted their password. This then depends on the salt as well.
    - Having an option to hide the fingerprint for scenarios where your screen might be monitored.
    - Getting rid of the fingerprint.
    
    There are issues with all these approaches. Removing the fingerprint provides the best security at the cost of usability. However, you often realise you've typed a password wrong, you may be able to recognise that the derived site password is wrong, and you could call Cahir twice (retyping the master password) to check that it's correct if you were unsure. Furthermore, the other parameters (e.g., your identity, the domain, the counter, the length, the character set, the pepper) could be wrong and the fingerprint doesn't help you detect this. This is an inherent flaw with stateless deterministic password managers.

--- src/Cahir/PasswordEntry.cs
+++ src/Cahir/PasswordEntry.cs
@@ -1,5 +1,4 @@
 ﻿using static Monocypher.Monocypher;
-using System.Buffers.Binary;
 using System.Text;
 
 namespace Cahir;
@@ -12,12 +11,10 @@ public static class PasswordEntry
         int count = 0;
         Span<char> password = stackalloc char[Constants.MaxPasswordChars];
         fixed (char* p = password) {
-            string[] wordlist = Resources.wordlist.Split(separator: ["\n"], StringSplitOptions.RemoveEmptyEntries);
             ConsoleKeyInfo pressedKey;
             do {
                 pressedKey = Console.ReadKey(intercept: true);
                 if (count >= Constants.MaxPasswordChars) {
-                    Console.Write($"\r{new string(' ', Console.BufferWidth)}\r");
                     throw new ArgumentException($"The password must be at most {Constants.MaxPasswordChars} characters long.");
                 }
                 if(!char.IsControl(pressedKey.KeyChar)) {
@@ -28,10 +25,7 @@ public static class PasswordEntry
                     count--;
                     password[count] = '\0';
                 }
-                Fingerprint(passwordBuffer, password[..count], wordlist);
             } while (pressedKey.Key != ConsoleKey.Enter);
-            crypto_wipe(passwordBuffer);
-            Console.Write($"\r{new string(' ', Console.BufferWidth)}\r");
             if (count == 0) {
                 throw new ArgumentException("You didn't enter a password.");
             }
@@ -39,25 +33,6 @@ public static class PasswordEntry
             crypto_wipe(new IntPtr(p), password.Length * sizeof(char));
             return passwordLength;
         }
-    }
-
-    private static void Fingerprint(Span<byte> passwordBuffer, ReadOnlySpan<char> typedPassword, IReadOnlyList<string> wordlist)
-    {
-        int passwordLength = Encoding.UTF8.GetBytes(typedPassword, passwordBuffer);
-        Span<byte> fingerprint = stackalloc byte[Constants.FingerprintSize];
-        var ctx = new crypto_blake2b_ctx();
-        crypto_blake2b_init(ref ctx, fingerprint.Length);
-        crypto_blake2b_update(ref ctx, "cahir.fingerprint"u8);
-        crypto_blake2b_update(ref ctx, passwordBuffer[..passwordLength]);
-        crypto_blake2b_final(ref ctx, fingerprint);
-
-        var index0 = BinaryPrimitives.ReadUInt128LittleEndian(fingerprint[..16]) % (UInt128)wordlist.Count;
-        var index1 = BinaryPrimitives.ReadUInt128LittleEndian(fingerprint[16..32]) % (UInt128)wordlist.Count;
-        var index2 = BinaryPrimitives.ReadUInt128LittleEndian(fingerprint[32..48]) % (UInt128)wordlist.Count;
-        var index3 = BinaryPrimitives.ReadUInt128LittleEndian(fingerprint[48..]) % (UInt128)wordlist.Count;
-        Console.Write($"\r{new string(' ', Console.BufferWidth)}\r");
-        Console.Write($"{wordlist[(int)index0]}-{wordlist[(int)index1]}-{wordlist[(int)index2]}-{wordlist[(int)index3]}".Replace("\r", ""));
-        crypto_wipe(fingerprint);
     }
 
     public static int ReadPasswordFile(Span<byte> passwordBuffer, string path)
